484c65ce2ae3235901b42e2f5c4e13b00b5faeaa
James Kelly Espera <esperajameskelly123@gmail.com>
2026-07-13 11:31:43 +0800

    Residents: page-size options in steps of 10 (10/20/30/40/50)

--- src/pages/Residents.jsx
+++ src/pages/Residents.jsx
@@ -680,7 +680,9 @@ export default function Residents() {
                 style={{ fontSize: 12, padding: '4px 8px' }}
               >
                 <option value={10}>10</option>
-                <option value={25}>25</option>
+                <option value={20}>20</option>
+                <option value={30}>30</option>
+                <option value={40}>40</option>
                 <option value={50}>50</option>
               </select>
             </div>
